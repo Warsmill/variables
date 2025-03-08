e4626983ea429966a750f921a4d60d0b31b8c1e8
warsmill <warsmill@yandex.ru>
2025-03-08 23:36:07 +0300

    task2

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,12 @@ public class Main {
         System.out.println(dog);
         System.out.println(cat);
         System.out.println(paper);
-
+            //задание 2
+        var dog1 = dog + 4;
+        var cat1 = cat + 4;
+        var paper1 = paper + 4;
+        System.out.println(dog1);
+        System.out.println(cat1);
+        System.out.println(paper1);
     }
 }
